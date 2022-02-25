5958cbe1c076c59b054f498673a79a4fa324e277
Mike <mike@prime31.com>
2022-02-24 17:28:51 -0800

    macos build and package

--- build_macos.sh
+++ build_macos.sh
@@ -1,4 +1,10 @@
-mkdir build
+[ -d build ] || mkdir build
 cd build
 cmake ..
-make -j16
+make -j8
+
+# package the macOS app bundle
+cd ..
+[ -d "build/SHADERed.app" ] || cp -R Misc/macBundleTemplate/SHADERed.app build/bin
+cp -a bin/ build/bin/SHADERed.app/Contents/MacOS
+cp -a build/bin/SHADERed build/bin/SHADERed.app/Contents/MacOS
